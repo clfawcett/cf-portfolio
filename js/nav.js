720d0e5566db7c276a57d2f34959688c6e938e0c
Casey Fawcett <caseyfawcett@gmail.com>
2018-11-29 09:48:20 -0800

    Mobile menu complete

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+var menuDiv = document.getElementById("menu_links");
+
+function openNavMenu() {
+  menuDiv.style.display = "block";
+}
+
+function closeNavMenu() {
+  menuDiv.style.display = "none";
+}
